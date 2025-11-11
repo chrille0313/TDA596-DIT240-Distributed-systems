077ed6a8fadee02c16fc674cd4d68e0aad77229d
Christian <chrille_0313@hotmail.com>
2025-11-11 12:26:50 +0100

    refactor: simplify + add comment

--- Labs/Lab1/http/response_builder.go
+++ Labs/Lab1/http/response_builder.go
@@ -24,6 +24,7 @@ func (builder *ResponseBuilder) Ok() *ResponseBuilder {
 	return builder.Status(Ok)
 }
 
+// TODO: allow multiple values for the same key
 func (builder *ResponseBuilder) Header(key string, value string) *ResponseBuilder {
 	builder.response.Headers[key] = []string{value}
 	return builder
@@ -31,13 +32,9 @@ func (builder *ResponseBuilder) Header(key string, value string) *ResponseBuilde
 
 // TODO: adapt body + content-type to the actual type
 func (builder *ResponseBuilder) Body(body string) *ResponseBuilder {
-	if body == "" {
-		return builder
-	}
-
 	builder.response.Body = body
+	builder.response.Headers["Content-Type"] = []string{"text/plain"}
 	builder.response.Headers["Content-Length"] = []string{strconv.Itoa(len(body))}
-	builder.response.Headers["Content-Type"] = []string{"text/plain"} 
 	return builder
 }
 
