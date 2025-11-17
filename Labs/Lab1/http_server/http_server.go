725a7acc176b50ddcd04a62d2924e73a14f9fe74
Christian <chrille_0313@hotmail.com>
2025-11-17 22:52:43 +0100

    Use default http port (80)

--- Labs/Lab1/http_server/http_server.go
+++ Labs/Lab1/http_server/http_server.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	host := flag.String("host", "0.0.0.0", "Host to bind to")
-	port := flag.String("port", "8080", "Port to listen on")
+	port := flag.String("port", "80", "Port to listen on")
 	portAlias := flag.String("p", "", "Alias for -port")
 	maxConnections := flag.Uint("maxConnections", 10, "Maximum concurrent connections")
 
@@ -52,7 +52,7 @@ func main() {
 	server.Post(func(r *nethttp.Request, rb *http.ResponseBuilder) error {
 		defer r.Body.Close()
 
-		err := writeFile(r.URL.Path, r.Body);
+		err := writeFile(r.URL.Path, r.Body)
 		if err != nil {
 			return &http.HTTPError{Status: statusFromFileError(err)}
 		}
