e44bdb179e699b131cc7b851cfe84e3135c4a51b
Christian <chrille_0313@hotmail.com>
2025-11-17 23:03:38 +0100

    fix: clean up proxy code and fix issues

--- Labs/Lab1/proxy/proxy.go
+++ Labs/Lab1/proxy/proxy.go
@@ -4,17 +4,15 @@ import (
 	"Lab1/http"
 	"bufio"
 	"flag"
-	"fmt"
 	"io"
 	"net"
-	builtInHttp "net/http"
+	nethttp "net/http"
 	"strings"
 )
 
 func main() {
 	host := flag.String("host", "0.0.0.0", "Host to bind to")
-	port := flag.String("port", "8080", "Port to listen on")
-	origin := flag.String("origin", "localhost:8080", "The server to proxy incoming requests to")
+	port := flag.String("port", "80", "Port to listen on")
 	portAlias := flag.String("p", "", "Alias for -port")
 	maxConnections := flag.Uint("maxConnections", 10, "Maximum concurrent connections")
 
@@ -31,37 +29,54 @@ func main() {
 	address := net.JoinHostPort(*host, *port)
 	server := http.NewServer()
 
-	server.Get(func(request *builtInHttp.Request, rb *http.ResponseBuilder) error {
-		connection, err := net.Dial("tcp", *origin)
-		if err != nil {
-			return err
-		}
-		defer connection.Close()
-
-		req := request.Method + " " + request.URL.Path + " " + request.Proto + "\r\nHost: " + address + "\r\n\r\n"
-		fmt.Println(req)
-
-		connection.Write([]byte(req))
-
-		response, err := builtInHttp.ReadResponse(bufio.NewReader(connection), nil)
-		if err != nil {
-			return err
-		}
-
-		defer response.Body.Close()
-
-		body, err := io.ReadAll(response.Body)
-		if err != nil {
-			return err
-		}
-
-		rb.Status(http.StatusCode(response.StatusCode)).Bytes(body)
-		for headerKey, headerValue := range response.Header {
-			rb.Header(headerKey, strings.Join(headerValue, ","))
-		}
-
-		return nil
+	server.Get(func(request *nethttp.Request, rb *http.ResponseBuilder) error {
+		return handleProxyRequest(request, rb)
 	})
 
 	server.Listen(address, *maxConnections)
+}
+
+func handleProxyRequest(request *nethttp.Request, rb *http.ResponseBuilder) error {
+	targetHost := request.URL.Host
+	if targetHost == "" {
+		targetHost = request.Host
+	}
+	if targetHost == "" {
+		return &http.HTTPError{Status: http.BadRequest}
+	}
+	if !strings.Contains(targetHost, ":") {
+		targetHost += ":80"
+	}
+
+	connection, err := net.Dial("tcp", targetHost)
+	if err != nil {
+		return &http.HTTPError{Status: http.InternalServerError}
+	}
+	defer connection.Close()
+
+	request.Header.Del("Proxy-Connection")
+	request.RequestURI = ""
+
+	err = request.Write(connection)
+	if err != nil {
+		return &http.HTTPError{Status: http.InternalServerError}
+	}
+
+	response, err := nethttp.ReadResponse(bufio.NewReader(connection), request)
+	if err != nil {
+		return &http.HTTPError{Status: http.InternalServerError}
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return &http.HTTPError{Status: http.InternalServerError}
+	}
+
+	rb.Status(http.StatusCode(response.StatusCode)).Bytes(body)
+	for headerKey, headerValues := range response.Header {
+		rb.Header(headerKey, strings.Join(headerValues, ", "))
+	}
+
+	return nil
 }
