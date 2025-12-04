6527b4db39521ec7177c511d37890b16d9e5d6ea
Christian Mattsson <chrille0313@gmail.com>
2025-12-04 10:21:25 +0100

    debug

--- Labs/Lab2/6.5840/src/mr/coordinator_rpc.go
+++ Labs/Lab2/6.5840/src/mr/coordinator_rpc.go
@@ -38,6 +38,7 @@ func CallCoordinator(rpcname string, args interface{}, reply interface{}) bool {
 	defer client.Close()
 
 	if err := client.Call("Coordinator." + rpcname, args, reply); err != nil {
+		debugf("worker: failed to call coordinator at %s: %v", address, err)
 		return false
 	}
 
